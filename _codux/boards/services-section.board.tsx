360d2718fc770f70ef3d8b145a3d7da3928c0bff
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-10-08 16:50:42 +0300

    moved from common to board's stylesheet

--- _codux/boards/services-section.board.tsx
+++ _codux/boards/services-section.board.tsx
@@ -1,18 +1,24 @@
 import { createBoard } from '@wixc3/react-board';
-import commonStylesStyles from '../../src/styles/common-styles.module.scss';
 import classNames from 'classnames';
 import styles from './services-section.board.module.scss';
+import commonStylesStyles from '../../src/styles/common-styles.module.scss';
 
 export default createBoard({
     name: 'ServicesSection',
     Board: () => (
-        <div className={commonStylesStyles.servicesSection}>
-            <h2 className={commonStylesStyles.servicesTitle}> Write a Title Here</h2>
-            <p className={commonStylesStyles.servicesDescription}>
-                This is the space to introduce visitors to the business or brand. Briefly explain
-                who&apos;s behind it, what it does and what makes it unique. Share its core values
-                and what this site has to offer.
-            </p>
+        <div className={styles.servicesSection}>
+            <div className={styles.content}>
+                <h2 className={styles.servicesTitle}> Write a Title Here</h2>
+                <p className={styles.servicesDescription}>
+                    This is the space to introduce visitors to the business or brand. Briefly
+                    explain who&apos;s behind it, what it does and what makes it unique. Share its
+                    core values and what this site has to offer.
+                </p>
+                <button className={commonStylesStyles.secondaryButtonWidth}>Explore</button>
+            </div>
         </div>
     ),
+    environmentProps: {
+        windowWidth: 701,
+    },
 });
