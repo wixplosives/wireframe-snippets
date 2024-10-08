9145413427a2a97f343dd4b3cdd3511d56952ba5
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-10-08 17:24:39 +0300

    added cards section

--- _codux/boards/services-section.board.tsx
+++ _codux/boards/services-section.board.tsx
@@ -2,6 +2,10 @@ import { createBoard } from '@wixc3/react-board';
 import classNames from 'classnames';
 import styles from './services-section.board.module.scss';
 import commonStylesStyles from '../../src/styles/common-styles.module.scss';
+import LightningSvg from '../../src/assets/lightning.svg';
+import AnchorSvg from '../../src/assets/anchor.svg';
+import LeafSvg from '../../src/assets/leaf.svg';
+import GlobeSvg from '../../src/assets/globe.svg';
 
 export default createBoard({
     name: 'ServicesSection',
@@ -16,9 +20,55 @@ export default createBoard({
                 </p>
                 <button className={commonStylesStyles.secondaryButtonWidth}>Explore</button>
             </div>
+            <div className={styles.allCards}>
+                <div className={styles.card}>
+                    <div className={styles.iconContainer}>
+                        <img src={AnchorSvg} alt="" className={styles.icon} />
+                    </div>
+                    <h6 className={styles.serviceCardTitle}>Service Title</h6>
+                    <p className={styles.serviceCardDescription}>
+                        This is the space to describe the service and explain how customers or
+                        clients can benefit from it. It’s an opportunity to add a short description
+                        that includes relevant details.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <div className={styles.iconContainer}>
+                        <img src={LightningSvg} alt="" className={styles.icon} />
+                    </div>
+                    <h6 className={styles.serviceCardTitle}>Service Title</h6>
+                    <p className={styles.serviceCardDescription}>
+                        This is the space to describe the service and explain how customers or
+                        clients can benefit from it. It’s an opportunity to add a short description
+                        that includes relevant details.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <div className={styles.iconContainer}>
+                        <img src={GlobeSvg} alt="" className={styles.icon} />
+                    </div>
+                    <h6 className={styles.serviceCardTitle}>Service Title</h6>
+                    <p className={styles.serviceCardDescription}>
+                        This is the space to describe the service and explain how customers or
+                        clients can benefit from it. It’s an opportunity to add a short description
+                        that includes relevant details.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <div className={styles.iconContainer}>
+                        <img src={LeafSvg} alt="" className={styles.icon} />
+                    </div>
+                    <h6 className={styles.serviceCardTitle}>Service Title</h6>
+                    <p className={styles.serviceCardDescription}>
+                        This is the space to describe the service and explain how customers or
+                        clients can benefit from it. It’s an opportunity to add a short description
+                        that includes relevant details.
+                    </p>
+                </div>
+            </div>
         </div>
     ),
     environmentProps: {
-        windowWidth: 701,
+        windowWidth: 1363,
     },
 });
