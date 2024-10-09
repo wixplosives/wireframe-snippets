01a922ecbd82437e97ea195f5ffa656503b8435f
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-10-09 20:58:10 +0300

    align class names

--- _codux/boards/services-section.board.tsx
+++ _codux/boards/services-section.board.tsx
@@ -5,22 +5,22 @@ export default createBoard({
     name: 'ServicesSection',
     Board: () => (
         <div className="servicesSection">
-            <div className="content">
+            <div className="servicesContent">
                 <h2 className="servicesTitle"> Write a Title Here</h2>
                 <p className="servicesDescription">
                     This is the space to introduce visitors to the business or brand. Briefly
                     explain who&apos;s behind it, what it does and what makes it unique. Share its
                     core values and what this site has to offer.
                 </p>
-                <button className="button">Button</button>
+                <button className="servicesButton">Button</button>
             </div>
-            <div className="allCards">
-                <div className="card">
+            <div className="servicesCards">
+                <div className="card servicesCard">
                     <div className="iconContainer">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_d9c1368c073d449fb7f70d1f27cdd94a.svg"
                             alt=""
-                            className="icon"
+                            className="servicesCardIcon"
                         />
                     </div>
                     <h6 className="serviceCardTitle">Service Title</h6>
@@ -30,12 +30,12 @@ export default createBoard({
                         that includes relevant details.
                     </p>
                 </div>
-                <div className="card">
+                <div className="servicesCard">
                     <div className="iconContainer">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_81a4aae870434124b16c503e6ed35e04.svg"
                             alt=""
-                            className="icon"
+                            className="servicesCardIcon"
                         />
                     </div>
                     <h6 className="serviceCardTitle">Service Title</h6>
@@ -45,12 +45,12 @@ export default createBoard({
                         that includes relevant details.
                     </p>
                 </div>
-                <div className="card">
+                <div className="servicesCard">
                     <div className="iconContainer">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_db3d5faaafdb48d8acc17eaa13c45dca.svg"
                             alt=""
-                            className="icon"
+                            className="servicesCardIcon"
                         />
                     </div>
                     <h6 className="serviceCardTitle">Service Title</h6>
@@ -60,12 +60,12 @@ export default createBoard({
                         that includes relevant details.
                     </p>
                 </div>
-                <div className="card">
+                <div className="servicesCard">
                     <div className="iconContainer">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_e55d51b82da445fd890f374888fd9bea.svg"
                             alt=""
-                            className="icon"
+                            className="servicesCardIcon"
                         />
                     </div>
                     <h6 className="serviceCardTitle">Service Title</h6>
