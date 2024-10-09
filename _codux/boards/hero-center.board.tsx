714f9efa7d3cbf2508db9d62aa780084ae81001f
Codux <codux@codux.com>
2024-10-09 15:28:23 +0300

    fix file type and MQ

--- _codux/boards/hero-center.board.tsx
+++ _codux/boards/hero-center.board.tsx
@@ -1,25 +1,23 @@
 import { createBoard } from '@wixc3/react-board';
-import styles from './hero-center.board.module.scss';
-import commonStylesStyles from '../../src/styles/common-styles.module.scss';
-import classNames from 'classnames';
+import './hero-center.board.css';
 
 export default createBoard({
     name: 'HeroCenter',
     Board: () => (
-        <div className={styles.root}>
-            <h1 className={styles.title}>
+        <div className="root">
+            <h1 className="title">
                 Title Goes Here.
                 <br />
                 Click to Edit and Add Your Own Text.
             </h1>
-            <div className={styles.buttons}>
-                <button className={styles.secondaryButton}>Contact Us</button>
-                <button className={styles.primaryButton}>Learn More</button>
+            <div className="buttons">
+                <button className="secondaryButton">Contact Us</button>
+                <button className="primaryButton">Learn More</button>
             </div>
         </div>
     ),
     environmentProps: {
-        windowHeight: 768,
-        windowWidth: 1007,
+        windowHeight: 709,
+        windowWidth: 430,
     },
 });
