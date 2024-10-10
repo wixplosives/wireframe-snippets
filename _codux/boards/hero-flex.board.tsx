06ddabcb7729e9f93b70776669db689e341a1d10
Codux <codux@codux.com>
2024-10-10 14:56:26 +0300

    fix responsive

--- _codux/boards/hero-flex.board.tsx
+++ _codux/boards/hero-flex.board.tsx
@@ -4,23 +4,24 @@ import './hero-flex.board.css';
 export default createBoard({
     name: 'HeroFlex',
     Board: () => (
-        <div className="hero">
+        <div className="card">
             <img
-                className="heroImage"
-                src="https://static.wixstatic.com/media/8586f3_c98b193548784753bb357eeb03615da5~mv2.jpg/v1/fill/w_1728,h_1378/Image%20by%20Johannes%20Plenio.jpg"
+                src="https://static.wixstatic.com/media/8586f3_c98b193548784753bb357eeb03615da5~mv2.jpg/v1/fill/w_600,h_600/Image%20by%20Johannes%20Plenio.jpg"
                 alt=""
+                className="card-image"
             />
-            <div className="heroContent">
-                <h1 className="heroTitle">Write a Title Here</h1>
-                <p className="heroText">
+            <div className="card-content">
+                <h2>Write aTitle Here</h2>
+                <p>
                     This is a space to welcome visitors to the site. Grab their attention with copy
-                    that clearly states what the site is about, and add an engaging image or video.
+                    that clearly states what the site is about, and add an engaging image or video
                 </p>
-                <button className="heroButton">Explore</button>
+                <button className="button">Explore</button>
             </div>
         </div>
     ),
     environmentProps: {
-        windowWidth: 430,
+        windowWidth: 1124,
+        windowHeight: 1080,
     },
 });
