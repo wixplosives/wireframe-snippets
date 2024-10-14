ae913e6a7616967b049207c61dd5f1ba9aab41e8
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-10-14 17:11:31 +0300

    .

--- _codux/boards/services-section.board.tsx
+++ _codux/boards/services-section.board.tsx
@@ -2,74 +2,74 @@ import { createBoard } from '@wixc3/react-board';
 import './services-section.board.css';
 
 export default createBoard({
-    name: 'ServicesSection',
+    name: 'section',
     Board: () => (
-        <div className="servicesSection">
-            <div className="servicesContent">
-                <h2 className="servicesTitle"> Write a Title Here</h2>
-                <p className="servicesDescription ServicesSectionBoard_p2">
+        <div className="section">
+            <div className="section_content">
+                <h2 className="section_title"> Write a Title Here</h2>
+                <p className="section_description">
                     This is the space to introduce visitors to the business or brand. Briefly
                     explain who&apos;s behind it, what it does and what makes it unique. Share its
                     core values and what this site has to offer.
                 </p>
-                <button className="servicesButton">Button</button>
+                <button className="button">Button</button>
             </div>
-            <div className="servicesCards">
-                <div className="servicesCard">
-                    <div className="servicesCardIconContainer ">
+            <div className="card-grid">
+                <div className="card">
+                    <div className="card_icon-container">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_d9c1368c073d449fb7f70d1f27cdd94a.svg"
                             alt=""
-                            className="servicesCardIcon"
+                            className="card_icon"
                         />
                     </div>
-                    <h6 className="serviceCardTitle">Service Title</h6>
-                    <p className="serviceCardDescription">
+                    <h6 className="card_title">Service Title</h6>
+                    <p className="card_description">
                         This is the space to describe the service and explain how customers or
                         clients can benefit from it. It’s an opportunity to add a short description
                         that includes relevant details.
                     </p>
                 </div>
-                <div className="servicesCard">
-                    <div className="servicesCardIconContainer ">
+                <div className="card">
+                    <div className="card_icon-container">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_81a4aae870434124b16c503e6ed35e04.svg"
                             alt=""
-                            className="servicesCardIcon"
+                            className="card_icon"
                         />
                     </div>
-                    <h6 className="serviceCardTitle">Service Title</h6>
-                    <p className="serviceCardDescription">
+                    <h6 className="card_title">Service Title</h6>
+                    <p className="card_description">
                         This is the space to describe the service and explain how customers or
                         clients can benefit from it. It’s an opportunity to add a short description
                         that includes relevant details.
                     </p>
                 </div>
-                <div className="servicesCard">
-                    <div className="servicesCardIconContainer ">
+                <div className="card">
+                    <div className="card_icon-container">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_db3d5faaafdb48d8acc17eaa13c45dca.svg"
                             alt=""
-                            className="servicesCardIcon"
+                            className="card_icon"
                         />
                     </div>
-                    <h6 className="serviceCardTitle">Service Title</h6>
-                    <p className="serviceCardDescription">
+                    <h6 className="card_title">Service Title</h6>
+                    <p className="card_description">
                         This is the space to describe the service and explain how customers or
                         clients can benefit from it. It’s an opportunity to add a short description
                         that includes relevant details.
                     </p>
                 </div>
-                <div className="servicesCard">
-                    <div className="servicesCardIconContainer ">
+                <div className="card">
+                    <div className="card_icon-container">
                         <img
                             src="https://static.wixstatic.com/shapes/27eba2_e55d51b82da445fd890f374888fd9bea.svg"
                             alt=""
-                            className="servicesCardIcon"
+                            className="card_icon"
                         />
                     </div>
-                    <h6 className="serviceCardTitle">Service Title</h6>
-                    <p className="serviceCardDescription">
+                    <h6 className="card_title">Service Title</h6>
+                    <p className="card_description">
                         This is the space to describe the service and explain how customers or
                         clients can benefit from it. It’s an opportunity to add a short description
                         that includes relevant details.
@@ -79,7 +79,7 @@ export default createBoard({
         </div>
     ),
     environmentProps: {
-        windowWidth: 1877,
+        windowWidth: 1728,
         windowHeight: 687,
     },
 });
