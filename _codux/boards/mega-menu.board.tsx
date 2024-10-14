e6af43fde6bea69bae0e8240626f7fe41d155e33
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-10-14 14:45:27 +0300

    .

--- _codux/boards/mega-menu.board.tsx
+++ _codux/boards/mega-menu.board.tsx
@@ -85,7 +85,7 @@ export default createBoard({
                 <div className="categoryTitle">Featured</div>
                 <a className="featuredItem">
                     <img
-                        src="https://static.wixstatic.com/media/27eba2_d68cbca76a1f41b0819f880581b5d3f6~mv2.png/v1/fill/w_360,h_360/27eba2_d68cbca76a1f41b0819f880581b5d3f6~mv2.png"
+                        src="https://plus.unsplash.com/premium_photo-1670174693145-e4c5179ce1d0?q=80&amp;w=1964&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         className="featuredImg"
                     />
                     <div className="featuredText">
@@ -95,12 +95,20 @@ export default createBoard({
                 </a>
                 <a className="featuredItem">
                     <img
-                        src="https://static.wixstatic.com/media/27eba2_d68cbca76a1f41b0819f880581b5d3f6~mv2.png/v1/fill/w_360,h_360/27eba2_d68cbca76a1f41b0819f880581b5d3f6~mv2.png"
+                        src="https://plus.unsplash.com/premium_photo-1670530034951-b7d0428d6c9c?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOHx8fGVufDB8fHx8fA%3D%3D"
                         className="featuredImg"
                     />
                     <div className="featuredText">
-                        Article about AI
-                        <p className="itemDescription">Read more</p>
+                        Article about This<p className="itemDescription">Read more</p>
+                    </div>
+                </a>
+                <a className="featuredItem">
+                    <img
+                        src="https://images.unsplash.com/photo-1708797851367-202c252c31f1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        className="featuredImg"
+                    />
+                    <div className="featuredText">
+                        Article about That<p className="itemDescription">Read more</p>
                     </div>
                 </a>
             </div>
@@ -108,6 +116,6 @@ export default createBoard({
     ),
     environmentProps: {
         windowHeight: 565,
-        windowWidth: 1434,
+        windowWidth: 1594,
     },
 });
