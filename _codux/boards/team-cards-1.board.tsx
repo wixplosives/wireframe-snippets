afa7f39399b8c44ce07caf438bf7ec80c4484623
Codux <codux@codux.com>
2024-10-15 13:37:35 +0300

    create team cards 1 board

---
+++ _codux/boards/team-cards-1.board.tsx
@@ -0,0 +1,26 @@
+import './team-cards-1.board.css';
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'Team Cards 1',
+    Board: () => <div className="root">
+            <div className="imgBox">
+                <img
+                    src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_800/Frame%206.png"
+                    alt="Delicious Margherita Pizza"
+                    className="img"
+                />
+            </div>
+            <div className="contentBox">
+                <h1 className="title">Write a Title Here</h1>
+                <p className="description">
+                    This is a space to welcome visitors to the site. Grab their attention with copy
+                    that clearly states what the site is about, and add an engaging image or video.
+                </p>
+                <button className="exploreButton">Explore</button>
+            </div>
+        </div>,
+    environmentProps: {
+windowWidth: 2423
+}
+});
