97ae772edbc8f6d77836e8c3c127d0024096dae5
Codux <codux@codux.com>
2024-10-15 13:31:54 +0300

    fix the tsx

--- _codux/boards/hero-image.board.tsx
+++ _codux/boards/hero-image.board.tsx
@@ -4,12 +4,14 @@ import { createBoard } from '@wixc3/react-board';
 export default createBoard({
     name: 'Hero Image',
     Board: () => (
-        <div className="imgBox">
-            <img
-                src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_800/Frame%206.png"
-                alt="Delicious Margherita Pizza"
-                className="img"
-            />
+        <div className="root">
+            <div className="imgBox">
+                <img
+                    src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_800/Frame%206.png"
+                    alt="Delicious Margherita Pizza"
+                    className="img"
+                />
+            </div>
             <div className="contentBox">
                 <h1 className="title">Write a Title Here</h1>
                 <p className="description">
@@ -21,6 +23,6 @@ export default createBoard({
         </div>
     ),
     environmentProps: {
-        windowWidth: 1646,
+        windowWidth: 746,
     },
 });
