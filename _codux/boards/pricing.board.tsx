0cc5ded4c0d4b0e51567e9b76c53d25247205f94
Codux <codux@codux.com>
2024-10-30 15:31:49 +0200

    fix names

--- _codux/boards/pricing.board.tsx
+++ _codux/boards/pricing.board.tsx
@@ -5,11 +5,11 @@ export default createBoard({
     name: 'pricing',
     Board: () => (
         <div>
-            <div className="PricingBoard_teams">
+            <div className="pricings">
                 <div className="priceCard">
-                    <div className="PricingBoard_contentBox">
-                        <h2 className="teamTitle PricingBoard_header1">Basic Plan</h2>
-                        <h1 className="PricingBoard_header2">$19/mo</h1>
+                    <div className="contentBox">
+                        <h2 className="teamTitle header">Basic Plan</h2>
+                        <h1 className="priceText">$19/mo</h1>
                         <p className="description">Billed monthly</p>
                     </div>
                     <div className="content">
@@ -19,9 +19,7 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
                         <div className="PricingBoard_div1">
                             <img
@@ -29,9 +27,7 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
                         <div className="PricingBoard_div1">
                             <img
@@ -39,9 +35,7 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
                         <div className="PricingBoard_div1">
                             <img
@@ -49,67 +43,15 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
-                        <button className="exploreButton">Buy Now</button>
+                        <button className="button">Buy Now</button>
                     </div>
                 </div>
-                <div className="priceCard premium">
-                    <div className="PricingBoard_contentBox premium">
-                        <h2 className="teamTitle PricingBoard_header1 premium">premium Plan</h2>
-                        <h1 className="PricingBoard_header2">$19/mo</h1>
-                        <p className="description">Billed monthly</p>
-                    </div>
-                    <div className="content premium">
-                        <div className="PricingBoard_div1">
-                            <img
-                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
-                                alt=""
-                                className="PricingBoard_img1"
-                            />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
-                        </div>
-                        <div className="PricingBoard_div1">
-                            <img
-                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
-                                alt=""
-                                className="PricingBoard_img1"
-                            />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
-                        </div>
-                        <div className="PricingBoard_div1">
-                            <img
-                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
-                                alt=""
-                                className="PricingBoard_img1"
-                            />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
-                        </div>
-                        <div className="PricingBoard_div1">
-                            <img
-                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
-                                alt=""
-                                className="PricingBoard_img1"
-                            />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
-                        </div>
-                        <button className="exploreButton">Buy Now</button>
-                    </div>
-                </div>
-                <div className="priceCard">
-                    <div className="PricingBoard_contentBox">
-                        <h2 className="teamTitle PricingBoard_header1">free Plan</h2>
-                        <h1 className="PricingBoard_header2">$19/mo</h1>
+                <div className="premium">
+                    <div className="contentBox">
+                        <h2 className="teamTitle header">Basic Plan</h2>
+                        <h1 className="priceText">$19/mo</h1>
                         <p className="description">Billed monthly</p>
                     </div>
                     <div className="content">
@@ -119,9 +61,7 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
                         <div className="PricingBoard_div1">
                             <img
@@ -129,9 +69,7 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
                         <div className="PricingBoard_div1">
                             <img
@@ -139,9 +77,7 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
                         <div className="PricingBoard_div1">
                             <img
@@ -149,18 +85,58 @@ export default createBoard({
                                 alt=""
                                 className="PricingBoard_img1"
                             />
-                            <p className="PricingBoard_description PricingBoard_p1">
-                                This is a text for a feature plan
-                            </p>
+                            <p className="list">This is a text for a feature plan</p>
                         </div>
-                        <button className="exploreButton">Buy Now</button>
+                        <button className="button">Buy Now</button>
+                    </div>
+                </div>
+                <div className="priceCard">
+                    <div className="contentBox">
+                        <h2 className="teamTitle header">Basic Plan</h2>
+                        <h1 className="priceText">$19/mo</h1>
+                        <p className="description">Billed monthly</p>
+                    </div>
+                    <div className="content">
+                        <div className="PricingBoard_div1">
+                            <img
+                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
+                                alt=""
+                                className="PricingBoard_img1"
+                            />
+                            <p className="list">This is a text for a feature plan</p>
+                        </div>
+                        <div className="PricingBoard_div1">
+                            <img
+                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
+                                alt=""
+                                className="PricingBoard_img1"
+                            />
+                            <p className="list">This is a text for a feature plan</p>
+                        </div>
+                        <div className="PricingBoard_div1">
+                            <img
+                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
+                                alt=""
+                                className="PricingBoard_img1"
+                            />
+                            <p className="list">This is a text for a feature plan</p>
+                        </div>
+                        <div className="PricingBoard_div1">
+                            <img
+                                src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
+                                alt=""
+                                className="PricingBoard_img1"
+                            />
+                            <p className="list">This is a text for a feature plan</p>
+                        </div>
+                        <button className="button">Buy Now</button>
                     </div>
                 </div>
             </div>
         </div>
     ),
     environmentProps: {
-        windowWidth: 1428,
+        windowWidth: 1348,
         windowHeight: 1080,
     },
 });
