dfaf02d5722a754c3fe02617412692df24eb9d52
Daniel Cohen <danielcoh@wix.com>
2024-11-03 12:14:25 +0200

    revert: Restore previously removed files
    Restored hero-center.board.tsx/css, hero-flex.board.tsx,  services-section.board.tsx after miscommunication about deletion.

---
+++ _codux/boards/hero-center.board.tsx
@@ -0,0 +1,27 @@
+import { createBoard } from '@wixc3/react-board';
+import './hero-center.board.css';
+
+export default createBoard({
+    name: 'HeroCenter',
+    Board: () => (
+        <div className="hero">
+            <h1 className="heroTitle">
+                Title Goes Here.
+                <br />
+                Click to Edit and Add Your Own Text.
+            </h1>
+            <div className="heroButtons">
+                <a href="#a" className="heroSecondaryButton">
+                    Contact Us
+                </a>
+                <a href="#a" className="heroPrimaryButton">
+                    Learn More
+                </a>
+            </div>
+        </div>
+    ),
+    environmentProps: {
+        windowHeight: 400,
+        windowWidth: 600,
+    },
+});	
