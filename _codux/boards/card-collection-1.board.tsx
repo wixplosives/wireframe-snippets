31c3c73d48bf3aff02b87ed5712a52401097268a
Daniel Cohen <danielcoh@wix.com>
2024-11-04 10:31:58 +0200

    fix: fixes according to cr

--- _codux/boards/card-collection-1.board.tsx
+++ _codux/boards/card-collection-1.board.tsx
@@ -14,7 +14,7 @@ export default createBoard({
                     <div className="card-image-box">
                         <img
                             src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
-                            alt="Delicious Margherita Pizza"
+                            alt="Add a description of the image"
                             className="card-image"
                         />
                     </div>
