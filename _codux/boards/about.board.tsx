610f4d17e812f38b881eba3260212f66052b8ead
Daniel Cohen <danielcoh@wix.com>
2024-11-05 14:35:40 +0200

    refactor: remove old about grid component and styles and added new ones.

--- _codux/boards/about.board.tsx
+++ _codux/boards/about.board.tsx
@@ -1,25 +1,25 @@
-import './about-grid.board.css';
+import './about.board.css';
 import { createBoard } from '@wixc3/react-board';
 
 export default createBoard({
-    name: 'About Grid',
+    name: 'About',
     Board: () => (
-        <div className="about-grid">
-            <div className="about-grid-image-cell">
+        <div className="about">
+            <div className="aboutImageCell">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_960,h_689,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/Frame%206.png"
                     alt=""
-                    className="about-grid-image"
+                    className="aboutImage"
                 />
             </div>
-            <div className="about-grid-title-cell">
-                <h1 className="about-grid-title">Write a Title Here</h1>
+            <div className="aboutTitleCell">
+                <h1 className="aboutTitle">Write a Title Here</h1>
             </div>
-            <div className="about-grid-subtitle-cell">
-                <h5 className="about-grid-subtitle">Subtitle Goes Here</h5>
+            <div className="aboutSubtitleCell">
+                <h5 className="aboutSubtitle">Subtitle Goes Here</h5>
             </div>
-            <div className="about-grid-content-cell">
-                <p className="about-grid-content">
+            <div className="aboutContentCell">
+                <p className="aboutContent">
                     This is a paragraph where you can include any information you’d like. It’s an
                     opportunity to tell a story about the company, describe a special service it
                     offers, or highlight a particular feature that sets it apart from competitors.
@@ -27,20 +27,20 @@ export default createBoard({
                     size or scale to customize the style.
                 </p>
             </div>
-            <div className="about-grid-button-cell">
-                <button className="about-grid-button">Button</button>
+            <div className="aboutButtonCell">
+                <button className="aboutButton">Button</button>
             </div>
-            <div className="about-grid-second-image-cell">
+            <div className="aboutSecondImageCell">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_960,h_689,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/Frame%206.png"
                     alt=""
-                    className="about-grid-image"
+                    className="aboutImage"
                 />
             </div>
         </div>
     ),
     environmentProps: {
-        windowWidth: 1007,
+        windowWidth: 957.6666666666666,
         windowHeight: 747,
     },
 });
