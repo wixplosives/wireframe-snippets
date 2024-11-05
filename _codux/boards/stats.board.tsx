629495bbac4463eab3150822c68454a4ba2875f9
Daniel Cohen <danielcoh@wix.com>
2024-11-05 18:51:37 +0200

    refactor: remove old stats section component and rename related CSS file

---
+++ _codux/boards/stats.board.tsx
@@ -0,0 +1,37 @@
+import './stats.board.css';
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'Stats',
+    Board: () => (
+        <div className="stats">
+            <h2 className="statsHeader">Our Story in Numbers</h2>
+            <div className="statsAllStatsContainer">
+                <div className="statsContainer">
+                    <h4 className="statsNumber">30K</h4>
+                    <h4 className="statsTitle">Item Title</h4>
+                    <p className="statsDescription">Use this space to explain the above number.</p>
+                </div>
+                <div className="statsContainer">
+                    <h4 className="statsNumber">+27%</h4>
+                    <h4 className="statsTitle">Item Title</h4>
+                    <p className="statsDescription">Use this space to explain the above number.</p>
+                </div>
+                <div className="statsContainer">
+                    <h4 className="statsNumber">15M</h4>
+                    <h4 className="statsTitle">Item Title</h4>
+                    <p className="statsDescription">Use this space to explain the above number.</p>
+                </div>
+                <div className="statsContainer">
+                    <h4 className="statsNumber">890</h4>
+                    <h4 className="statsTitle">Item Title</h4>
+                    <p className="statsDescription">Use this space to explain the above number.</p>
+                </div>
+            </div>
+        </div>
+    ),
+    environmentProps: {
+        windowWidth: 980,
+        windowHeight: 1180,
+    },
+});
