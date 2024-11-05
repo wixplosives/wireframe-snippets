89b8465dbf3d6bd667a8c3d926070073e29e0777
Daniel Cohen <danielcoh@wix.com>
2024-11-05 15:44:14 +0200

    refactor: update teamCards classnames according to convention

--- _codux/boards/team-cards.board.tsx
+++ _codux/boards/team-cards.board.tsx
@@ -2,46 +2,46 @@ import './team-cards.board.css';
 import { createBoard } from '@wixc3/react-board';
 
 export default createBoard({
-    name: 'Team Cards',
+    name: 'teamCards',
     Board: () => (
         <div className="teamCards">
-            <div className="card">
+            <div className="teamCardsCard">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
                     alt="Featured Item"
-                    className="card-image"
+                    className="teamCardsCardImage"
                 />
-                <div className="card-content">
-                    <h1 className="card-title">Featured Item Two</h1>
-                    <p className="card-subtitle">
+                <div className="teamCardsCardContent">
+                    <h1 className="teamCardsCardTitle">Featured Item Two</h1>
+                    <p className="teamCardsCardSubtitle">
                         Add paragraph text. Click “Edit Text” to update the font, size and more. To
                         change and reuse text themes, go to Site Styles.
                     </p>
                 </div>
             </div>
-            <div className="card">
+            <div className="teamCardsCard">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
                     alt="Featured Item"
-                    className="card-image"
+                    className="teamCardsCardImage"
                 />
-                <div className="card-content">
-                    <h1 className="card-title">Featured Item Two</h1>
-                    <p className="card-subtitle">
+                <div className="teamCardsCardContent">
+                    <h1 className="teamCardsCardTitle">Featured Item Two</h1>
+                    <p className="teamCardsCardSubtitle">
                         Add paragraph text. Click “Edit Text” to update the font, size and more. To
                         change and reuse text themes, go to Site Styles.
                     </p>
                 </div>
             </div>
-            <div className="card">
+            <div className="teamCardsCard">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
                     alt="Featured Item"
-                    className="card-image"
+                    className="teamCardsCardImage"
                 />
-                <div className="card-content">
-                    <h1 className="card-title">Featured Item Two</h1>
-                    <p className="card-subtitle">
+                <div className="teamCardsCardContent">
+                    <h1 className="teamCardsCardTitle">Featured Item Two</h1>
+                    <p className="teamCardsCardSubtitle">
                         Add paragraph text. Click “Edit Text” to update the font, size and more. To
                         change and reuse text themes, go to Site Styles.
                     </p>
@@ -50,7 +50,7 @@ export default createBoard({
         </div>
     ),
     environmentProps: {
-        windowWidth: 630,
-        windowHeight: 556,
+        windowWidth: 1082,
+        windowHeight: 768,
     },
 });
