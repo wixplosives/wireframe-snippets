13f4cfc60fd1acd9fb9156843945b9bc1195e49c
Daniel Cohen <danielcoh@wix.com>
2024-11-05 15:49:12 +0200

    refactor: rename card classnames to teamCards for consistency and clarity

--- _codux/boards/team-cards.board.tsx
+++ _codux/boards/team-cards.board.tsx
@@ -8,7 +8,7 @@ export default createBoard({
             <div className="teamCardsCard">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
-                    alt="Featured Item"
+                    alt=""
                     className="teamCardsCardImage"
                 />
                 <div className="teamCardsCardContent">
@@ -22,7 +22,7 @@ export default createBoard({
             <div className="teamCardsCard">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
-                    alt="Featured Item"
+                    alt=""
                     className="teamCardsCardImage"
                 />
                 <div className="teamCardsCardContent">
@@ -36,7 +36,7 @@ export default createBoard({
             <div className="teamCardsCard">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
-                    alt="Featured Item"
+                    alt=""
                     className="teamCardsCardImage"
                 />
                 <div className="teamCardsCardContent">
