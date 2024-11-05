5c151f001ced034d3acddad1e923652fa60b547c
Daniel Cohen <danielcoh@wix.com>
2024-11-05 11:57:51 +0200

    fix: renamed class names according to BEM methodology and removed unnecessary css declarations

--- _codux/boards/clients-section.board.tsx
+++ _codux/boards/clients-section.board.tsx
@@ -4,70 +4,70 @@ import { createBoard } from '@wixc3/react-board';
 export default createBoard({
     name: 'Clients Section',
     Board: () => (
-        <div className="clientsSection">
-            <div className="clientsContent">
-                <div className="clientsContentTexts">
-                    <h2 className="clientsTitle">Our Clients</h2>
-                    <p className="clientsDescription">
+        <div className="clients-section">
+            <div className="clients-section__content">
+                <div className="clients-section__texts">
+                    <h2 className="clients-section__title">Our Clients</h2>
+                    <p className="clients-section__description">
                         Showcase current and previous clients and their success stories. Use this
                         space to share information about your partnership and add examples of shared
                         projects and how they made an impact.
                     </p>
                 </div>
                 <img
-                    src="https://static.wixstatic.com/media/baac51_85ea66dc04284f40910abb8e1b90a2ba~mv2.jpg/v1/fill/w_1680,h_954,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/baac51_85ea66dc04284f40910abb8e1b90a2ba~mv2.jpg"
+                    src="https://static.wixstatic.com/media/8586f3_c98b193548784753bb357eeb03615da5~mv2.jpg/v1/fill/w_1680,h_954/Image%20by%20Johannes%20Plenio.jpg"
                     alt=""
-                    className="clientsSectionImage"
+                    className="clients-section__image"
                 />
             </div>
-            <div className="clientsLogosGrid">
-                <div className="clientLogoContainer">
+            <div className="clients-section__logos-container">
+                <div className="clients-section__logo-container">
                     <img
                         src="https://static.wixstatic.com/shapes/27eba2_5c720747d7354dba80018d7d997449dc.svg"
                         alt=""
-                        className="clientLogo"
+                        className="clients-section__logo-image"
                     />
                 </div>
-                <div className="clientLogoContainer">
+                <div className="clients-section__logo-container">
                     <img
                         src="https://static.wixstatic.com/shapes/27eba2_2166ae7dd24f43b5b5d81b82723fb514.svg"
                         alt=""
-                        className="clientLogo"
+                        className="clients-section__logo-image"
                     />
                 </div>
-                <div className="clientLogoContainer">
+                <div className="clients-section__logo-container">
                     <img
                         src="https://static.wixstatic.com/shapes/27eba2_8578952f0104450ab6439b62e80a4e94.svg"
                         alt=""
-                        className="clientLogo"
+                        className="clients-section__logo-image"
                     />
                 </div>
-                <div className="clientLogoContainer">
+                <div className="clients-section__logo-container">
                     <img
                         src="https://static.wixstatic.com/shapes/27eba2_b38de69a8fef47a4a7b4352eb614e56c.svg"
                         alt=""
-                        className="clientLogo"
+                        className="clients-section__logo-image"
                     />
                 </div>
-                <div className="clientLogoContainer">
+                <div className="clients-section__logo-container">
                     <img
                         src="https://static.wixstatic.com/shapes/27eba2_6f586dc3ae5c401f94331b8f43d80207.svg"
                         alt=""
-                        className="clientLogo"
+                        className="clients-section__logo-image"
                     />
                 </div>
-                <div className="clientLogoContainer">
+                <div className="clients-section__logo-container">
                     <img
                         src="https://static.wixstatic.com/shapes/27eba2_933967ebc7324eb1937d9d86b71dc875.svg"
                         alt=""
-                        className="clientLogo"
+                        className="clients-section__logo-image"
                     />
                 </div>
             </div>
         </div>
     ),
     environmentProps: {
-        windowWidth: 936,
-        windowHeight: 892,
+        windowWidth: 680,
+        windowHeight: 1024,
     },
 });
