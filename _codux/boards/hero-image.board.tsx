afaadacdb2255fd4b836fb4ac9ba56553fa4de3a
Daniel Cohen <danielcoh@wix.com>
2024-11-05 16:08:28 +0200

    refactor: rename hero image classnames for consistency and clarity

--- _codux/boards/hero-image.board.tsx
+++ _codux/boards/hero-image.board.tsx
@@ -4,21 +4,21 @@ import { createBoard } from '@wixc3/react-board';
 export default createBoard({
     name: 'Hero Image',
     Board: () => (
-        <div className="hero-image">
-            <div className="image-box">
+        <div className="heroImage">
+            <div className="heroImageImageBox">
                 <img
                     src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_800/Frame%206.png"
-                    alt="Delicious Margherita Pizza"
-                    className="img"
+                    alt=""
+                    className="heroImageImg"
                 />
             </div>
-            <div className="content-box">
-                <h1 className="title">Write a Title Here</h1>
-                <p className="description">
+            <div className="heroImageContentBox">
+                <h1 className="heroImageTitle">Write a Title Here</h1>
+                <p className="heroImageDescription">
                     This is a space to welcome visitors to the site. Grab their attention with copy
                     that clearly states what the site is about, and add an engaging image or video.
                 </p>
-                <button className="explore-button">Explore</button>
+                <button className="heroImageExploreButton">Explore</button>
             </div>
         </div>
     ),
