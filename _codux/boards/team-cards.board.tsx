3759e9322fc0b91ee9e352bf18321ab48065a5e9
Daniel Cohen <danielcoh@wix.com>
2024-11-06 20:23:22 +0200

    refactor: update team cards layout and styles, remove image elements and added the image as a background image

--- _codux/boards/team-cards.board.tsx
+++ _codux/boards/team-cards.board.tsx
@@ -2,55 +2,34 @@ import './team-cards.board.css';
 import { createBoard } from '@wixc3/react-board';
 
 export default createBoard({
-    name: 'teamCards',
+    name: 'Team Cards',
     Board: () => (
         <div className="teamCards">
             <div className="teamCardsCard">
-                <img
-                    src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
-                    alt=""
-                    className="teamCardsCardImage"
-                />
-                <div className="teamCardsCardContent">
-                    <h1 className="teamCardsCardTitle">Featured Item Two</h1>
-                    <p className="teamCardsCardSubtitle">
-                        Add paragraph text. Click “Edit Text” to update the font, size and more. To
-                        change and reuse text themes, go to Site Styles.
-                    </p>
-                </div>
+                <h1 className="teamCardsCardTitle">Team Card One</h1>
+                <p className="teamCardsCardSubtitle">
+                    Add paragraph text. Click “Edit Text” to update the font, size and more. To
+                    change and reuse text themes, go to Site Styles.
+                </p>
             </div>
             <div className="teamCardsCard">
-                <img
-                    src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
-                    alt=""
-                    className="teamCardsCardImage"
-                />
-                <div className="teamCardsCardContent">
-                    <h1 className="teamCardsCardTitle">Featured Item Two</h1>
-                    <p className="teamCardsCardSubtitle">
-                        Add paragraph text. Click “Edit Text” to update the font, size and more. To
-                        change and reuse text themes, go to Site Styles.
-                    </p>
-                </div>
+                <h1 className="teamCardsCardTitle">Team Card Two</h1>
+                <p className="teamCardsCardSubtitle">
+                    Add paragraph text. Click “Edit Text” to update the font, size and more. To
+                    change and reuse text themes, go to Site Styles.
+                </p>
             </div>
             <div className="teamCardsCard">
-                <img
-                    src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
-                    alt=""
-                    className="teamCardsCardImage"
-                />
-                <div className="teamCardsCardContent">
-                    <h1 className="teamCardsCardTitle">Featured Item Two</h1>
-                    <p className="teamCardsCardSubtitle">
-                        Add paragraph text. Click “Edit Text” to update the font, size and more. To
-                        change and reuse text themes, go to Site Styles.
-                    </p>
-                </div>
+                <h1 className="teamCardsCardTitle">Team Card Three</h1>
+                <p className="teamCardsCardSubtitle">
+                    Add paragraph text. Click “Edit Text” to update the font, size and more. To
+                    change and reuse text themes, go to Site Styles.
+                </p>
             </div>
         </div>
     ),
     environmentProps: {
-        windowWidth: 1082,
-        windowHeight: 768,
+        windowWidth: 1045,
+        windowHeight: 1080,
     },
 });
