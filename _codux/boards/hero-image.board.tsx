b46841f972cb43bf7710360cddfc7be54e0c486b
Daniel Cohen <danielcoh@wix.com>
2024-11-07 15:51:49 +0200

    refactor: update hero image layout and styles, adjust dimensions and remove unnecessary wrapper

--- _codux/boards/hero-image.board.tsx
+++ _codux/boards/hero-image.board.tsx
@@ -5,13 +5,11 @@ export default createBoard({
     name: 'Hero Image',
     Board: () => (
         <div className="heroImage">
-            <div className="heroImageImageBox">
-                <img
-                    src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_800/Frame%206.png"
-                    alt=""
-                    className="heroImageImg"
-                />
-            </div>
+            <img
+                src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_800/Frame%206.png"
+                alt=""
+                className="heroImageImg"
+            />
             <div className="heroImageContentBox">
                 <h1 className="heroImageTitle">Write a Title Here</h1>
                 <p className="heroImageDescription">
@@ -23,7 +21,7 @@ export default createBoard({
         </div>
     ),
     environmentProps: {
-        windowWidth: 1016,
-        windowHeight: 880,
+        windowWidth: 1024,
+        windowHeight: 768,
     },
 });
