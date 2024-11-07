cbca2fa66186cd6f5f9bdc0180f1d5ffe73ea2f4
Daniel Cohen <danielcoh@wix.com>
2024-11-07 10:28:47 +0200

    fix: removed all element wrapping divs exept the button. Simplified the css.

--- _codux/boards/about.board.tsx
+++ _codux/boards/about.board.tsx
@@ -10,35 +10,27 @@ export default createBoard({
                 alt=""
                 className="aboutImage"
             />
-            <div className="aboutTitleCell">
-                <h1 className="aboutTitleText">Write a Title Here</h1>
-            </div>
-            <div className="aboutSubtitleCell">
-                <h5 className="aboutSubtitleText">Subtitle Goes Here</h5>
-            </div>
-            <div className="aboutContentCell">
-                <p className="aboutContentText">
-                    This is a paragraph where you can include any information you’d like. It’s an
-                    opportunity to tell a story about the company, describe a special service it
-                    offers, or highlight a particular feature that sets it apart from competitors.
-                    Make sure it fits the general tone and voice of the brand, then adjust the font,
-                    size or scale to customize the style.
-                </p>
+            <h1 className="aboutTitleText">Write a Title Here</h1>
+            <h5 className="aboutSubtitleText">Subtitle Goes Here</h5>
+            <div className="aboutContentText">
+                This is a paragraph where you can include any information you’d like. It’s an
+                opportunity to tell a story about the company, describe a special service it offers,
+                or highlight a particular feature that sets it apart from competitors. Make sure it
+                fits the general tone and voice of the brand, then adjust the font, size or scale to
+                customize the style.
             </div>
             <div className="aboutButtonCell">
                 <button className="aboutButton">Button</button>
             </div>
-            <div className="aboutSecondImageCell">
-                <img
-                    src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_960,h_689,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/Frame%206.png"
-                    alt=""
-                    className="aboutImage"
-                />
-            </div>
+            <img
+                src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_960,h_689,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/Frame%206.png"
+                alt=""
+                className="aboutSecondImage"
+            />
         </div>
     ),
     environmentProps: {
-        windowWidth: 1024,
-        windowHeight: 768,
+        windowWidth: 718,
+        windowHeight: 1216,
     },
 });
