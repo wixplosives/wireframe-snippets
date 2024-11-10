2bc250a3d465b93e7ca5a4cfed61d54a0307a4cc
Daniel Cohen <danielcoh@wix.com>
2024-11-10 12:23:55 +0200

    cr chenges to team cards

--- _codux/boards/team-cards.board.tsx
+++ _codux/boards/team-cards.board.tsx
@@ -4,32 +4,32 @@ import { createBoard } from '@wixc3/react-board';
 export default createBoard({
     name: 'Team Cards',
     Board: () => (
-        <div className="teamCards">
-            <div className="teamCardsCard">
-                <h1 className="teamCardsCardTitle">Team Card One</h1>
-                <p className="teamCardsCardSubtitle">
-                    Double click the the text to edit its content, to edit font size, font colors,
-                    and all css properties in the style panel.
-                </p>
+            <div className="teamCards">
+                <div className="teamCardsCard">
+                    <h1 className="teamCardsCardTitle">Team Card One</h1>
+                    <p className="teamCardsCardSubtitle">
+                        Double click the the text to edit its content, to edit font size, font
+                        colors, and all css properties in the style panel.
+                    </p>
+                </div>
+                <div className="teamCardsCard">
+                    <h1 className="teamCardsCardTitle">Team Card Two</h1>
+                    <p className="teamCardsCardSubtitle">
+                        Double click the the text to edit its content, to edit font size, font
+                        colors, and all css properties in the style panel.
+                    </p>
+                </div>
+                <div className="teamCardsCard">
+                    <h1 className="teamCardsCardTitle">Team Card Three</h1>
+                    <p className="teamCardsCardSubtitle">
+                        Double click the the text to edit its content, to edit font size, font
+                        colors, and all css properties in the style panel.
+                    </p>
+                </div>
             </div>
-            <div className="teamCardsCard">
-                <h1 className="teamCardsCardTitle">Team Card Two</h1>
-                <p className="teamCardsCardSubtitle">
-                    Double click the the text to edit its content, to edit font size, font colors,
-                    and all css properties in the style panel.
-                </p>
-            </div>
-            <div className="teamCardsCard">
-                <h1 className="teamCardsCardTitle">Team Card Three</h1>
-                <p className="teamCardsCardSubtitle">
-                    Double click the the text to edit its content, to edit font size, font colors,
-                    and all css properties in the style panel.
-                </p>
-            </div>
-        </div>
     ),
     environmentProps: {
-        windowWidth: 1045,
-        windowHeight: 1080,
+        windowWidth: 1024,
+        windowHeight: 768,
     },
 });
