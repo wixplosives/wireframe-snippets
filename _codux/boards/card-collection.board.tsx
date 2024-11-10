8283292f79c5b9f11c653fe0efa0dc90c6bcd33f
Daniel Cohen <danielcoh@wix.com>
2024-11-10 11:53:31 +0200

    card collection cr changes

--- _codux/boards/card-collection.board.tsx
+++ _codux/boards/card-collection.board.tsx
@@ -14,7 +14,6 @@ export default createBoard({
                     <img
                         src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
                         alt=""
-                        className="cardCollectionImage"
                     />
                     <div className="cardCollectionContent">
                         <h2 className="cardCollectionTitle">Item Title</h2>
@@ -28,7 +27,6 @@ export default createBoard({
                     <img
                         src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
                         alt=""
-                        className="cardCollectionImage"
                     />
                     <div className="cardCollectionContent">
                         <h2 className="cardCollectionTitle">Item Title</h2>
@@ -42,7 +40,6 @@ export default createBoard({
                     <img
                         src="https://static.wixstatic.com/media/8586f3_f6d2538ac6374f71b6dc29511c5cd1a3~mv2.png/v1/fill/w_860,h_600/Frame%206.png"
                         alt=""
-                        className="cardCollectionImage"
                     />
                     <div className="cardCollectionContent">
                         <h2 className="cardCollectionTitle">Item Title</h2>
