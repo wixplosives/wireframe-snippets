4c20f3aaa4632e51e37e6f793461eb6cfd4152b9
Daniel Cohen <88962351+DanielGovrin@users.noreply.github.com>
2024-11-10 12:49:17 +0200

    refactor: refactor pricing plans (#16)

--- _codux/boards/pricing.board.tsx
+++ _codux/boards/pricing.board.tsx
@@ -5,139 +5,139 @@ export default createBoard({
     name: 'pricing',
     Board: () => (
         <div className="pricing">
-            <h1 className="pricing-title">Pricing Plans</h1>
-            <div className="pricing-cards">
-                <div className="pricing-card">
-                    <div className="pricing-header">
-                        <h2 className="pricing-header-title">New Client Trial</h2>
-                        <h1 className="pricing-header-text">$150</h1>
-                        <p className="pricing-header-description">Valid for 3 months</p>
+            <h1 className="pricingTitle">Pricing Plans</h1>
+            <div className="pricingCards">
+                <div className="pricingCard">
+                    <div className="pricingPlanOverview">
+                        <h2 className="pricingHeaderTitle">New Client Trial</h2>
+                        <h1 className="pricingHeaderCost">$150</h1>
+                        <p className="pricingHeaderDescription">Valid for 3 months</p>
                     </div>
-                    <div className="pricing-content">
-                        <div className="pricing-feature">
+                    <ul className="pricingPlanFeatures">
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <button className="pricing-button">Buy Now</button>
-                    </div>
+                            <p>Describe the above</p>
+                        </li>
+                    </ul>
+                    <button className="pricingButton">Buy Now</button>
                 </div>
-                <div className="pricing-premium">
-                    <div className="pricing-premium-header">
-                        <h2 className="pricing-header-title">Weekly Consulting</h2>
-                        <h1 className="pricing-header-text">$200</h1>
-                        <p className="pricing-premium-description">Valid for 12 months</p>
+                <div className="pricingCard">
+                    <div className="pricingPlanOverview">
+                        <h2 className="pricingHeaderTitle">Weekly Consulting</h2>
+                        <h1 className="pricingHeaderCost">$200</h1>
+                        <p className="pricingHeaderDescription">Valid for 12 months</p>
                     </div>
-                    <div className="pricing-content">
-                        <div className="pricing-feature">
+                    <ul className="pricingPlanFeatures">
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <button className="pricing-button">Buy Now</button>
-                    </div>
+                            <p>Describe the above</p>
+                        </li>
+                    </ul>
+                    <button className="pricingButton">Buy Now</button>
                 </div>
-                <div className="pricing-card">
-                    <div className="pricing-header">
-                        <h2 className="pricing-header-title">New Client Trial</h2>
-                        <h1 className="pricing-header-text">$150</h1>
-                        <p className="pricing-header-description">Valid for 3 months</p>
+                <div className="pricingCard">
+                    <div className="pricingPlanOverview">
+                        <h2 className="pricingHeaderTitle">New Client Trial</h2>
+                        <h1 className="pricingHeaderCost">$150</h1>
+                        <p className="pricingHeaderDescription">Valid for 3 months</p>
                     </div>
-                    <div className="pricing-content">
-                        <div className="pricing-feature">
+                    <ul className="pricingPlanFeatures">
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <div className="pricing-feature">
+                            <p>Describe the above</p>
+                        </li>
+                        <li className="pricingFeature">
                             <img
                                 src="https://static.wixstatic.com/media/13128652dab8451db4ce3e8b1b00d5b0.png/v1/fit/w_640,h_640/V Logo.png"
                                 alt="V Logo on Dark Background"
-                                className="pricing-img"
+                                className="pricingImage"
                             />
-                            <p className="pricing-feature">Describe the above</p>
-                        </div>
-                        <button className="pricing-button">Buy Now</button>
-                    </div>
+                            <p>Describe the above</p>
+                        </li>
+                    </ul>
+                    <button className="pricingButton">Buy Now</button>
                 </div>
             </div>
         </div>
     ),
     environmentProps: {
-        windowWidth: 874,
-        windowHeight: 832,
+        windowWidth: 942,
+        windowHeight: 768,
     },
 });
